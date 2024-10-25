ce84378c17e7f3d496fb74a0e6e0368e655d714e
Seth Verrill <verrill.seth@gmail.com>
2024-10-25 10:55:58 -0600

    test: Add error handling tests for GET :id

--- spec/requests/api/v1/posters_request_spec.rb
+++ spec/requests/api/v1/posters_request_spec.rb
@@ -124,4 +124,19 @@ RSpec.describe "Fetch all posters" do
 
         expect(posters.count).to eq(2)
      end
+
+     describe 'sad paths' do
+      it "can gracefully handle if a poster id does not exist" do
+        get "/api/v1/posters/1328922"
+
+        expect(response).to_not be_successful
+        expect(response.status).to eq(404)
+
+        data = JSON.parse(response.body, symbolize_names: true)
+
+        expect(data[:errors]).to be_a(Array)
+        expect(data[:errors].first[:status]).to eq("404")
+        expect(data[:errors].first)
+      end
+    end
 end     
