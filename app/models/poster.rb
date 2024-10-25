61290a96be40f621cb121fa8930b174d8a87bb46
Jonathan Atkins <jonathanjatkins89@gmail.com>
2024-10-25 09:44:24 -0700

    feat:updated min_max query

--- app/models/poster.rb
+++ app/models/poster.rb
@@ -1,5 +1,5 @@
 class Poster < ApplicationRecord
-# !validations/relationships/data_manipulation happen here
+    # !validations/relationships/data_manipulation happen here
     def self.sort_by_creation(choice)
         # *ActiveRecord method order that sorts the created_at attribute
         order(created_at: choice.to_sym)
@@ -7,5 +7,13 @@ class Poster < ApplicationRecord
 
     def self.i_like(choice)
         where("name ILIKE ?", "%#{choice}%")
+    end
+
+    def self.min_price(price)
+        where("price >= ?", price)
+    end    
+
+    def self.max_price(price)
+        where("price <= ?", price)
     end
 end
